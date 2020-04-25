45f9def4698d78b83c7f803677ef56d2439691fb
Mio Chung <miochung7@gmail.com>
2020-04-25 12:39:54 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links a');
+
+//     burger.addEventListener('click', () => {
+//         // Toggle Nav
+//         nav.classList.toggle('nav-active');
+
+//         //Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = ''
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards`;
+//             }
+//         });
+//     });
+// }
+
+// navSlide();
+
+
+
+
+
+// Mapbox
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlvY2h1bmc3IiwiYSI6ImNrOG13cXoxbDA2c2UzbW1lcm1iZWZ5NnEifQ.5nuyV8naVrjogYKyx_TFzw';
+
+
+
+const map = new mapboxgl.Map({
+    container: 'map', //appears in the container with the ID map
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-0.126240, 51.511580], // Starting position [lng, lat]
+    zoom: 11.89, // [Starting zoom]
+});
+
+
+
+// Custom Marker
+
+var marker = new mapboxgl.Marker() // initialize a new marker
+    .setLngLat([-0.126240, 51.511580]) // Marker [lng, lat] coordinates
+    .addTo(map); // Add the marker to the map
+
+// Geocode
+
+var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+    accessToken: mapboxgl.accessToken, // Set the access token
+    mapboxgl: mapboxgl, // Set the mapbox-gl instance
+    marker: false, // Do not use the default marker style
+    placeholder: '',
+    proximity: {
+        longitude: -0.126240,
+        latitude: 51.511580
+    }
+});
+
+// Add the geocoder to the map
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
+
+// Add zoom navigation control
+map.addControl(new mapboxgl.NavigationControl());
+
+window.addEventListener('resize', () => {
+    map.zoomTo(0);
+});
